f43a3648d433544078f1fcfb35d5ba25ca5a9c82
TylerBarrett-Werx <tbarrett@preswerx.com>
2024-10-01 14:31:30 -0500

    Enable each user to add their locations

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let currentUserId = 1;
+let currentMap = 1;
 
 let users = [
   { id: 1, name: "Together", color: "palevioletred" },
@@ -51,7 +52,7 @@ async function checkVisistedStates() {
   return states;
 }
 
-app.get("/", async (req, res) => {
+async function loadWorldMap(res) {
   const countries = await checkVisistedCountries();
   res.render("index.ejs", {
     countries: countries,
@@ -59,58 +60,92 @@ app.get("/", async (req, res) => {
     users: users,
     color: users[currentUserId - 1].color,
   });
+}
+
+async function loadStateMap(res) {
+  const states = await checkVisistedStates();
+  res.render("united_states.ejs", {
+    states: states,
+    total: states.length,
+    users: users,
+    color: users[currentUserId - 1].color,
+  });
+}
+
+app.get("/", async (req, res) => {
+  if (currentMap === 1) {
+    await loadWorldMap(res);
+  } else {
+    await loadStateMap(res);
+  }
 });
 
 app.post("/add", async (req, res) => {
-  const input = req.body["country"];
+  if (currentMap === 1) {
+    const input = req.body["country"];
 
-  try {
-    const result = await db.query(
-      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
-      [input.toLowerCase()]
-    );
-
-    const data = result.rows[0];
-    const countryCode = data.country_code;
     try {
-      await db.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1)",
-        [countryCode]
+      const result = await db.query(
+          "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+          [input.toLowerCase()]
       );
-      res.redirect("/");
+
+      const data = result.rows[0];
+      const countryCode = data.country_code;
+      try {
+        await db.query(
+            "INSERT INTO global_passports (user_id, country_id) SELECT $1, countries.id FROM countries WHERE countries.country_code = $2",
+            [currentUserId, countryCode]
+        );
+        res.redirect("/");
+      } catch (err) {
+        console.log(err);
+      }
     } catch (err) {
       console.log(err);
     }
-  } catch (err) {
-    console.log(err);
+  } else {
+    const input = req.body["state"];
+
+    try {
+      const result = await db.query(
+          "SELECT state_code FROM states WHERE LOWER(state_name) LIKE '%' || $1 || '%';",
+          [input.toLowerCase()]
+      );
+
+      const data = result.rows[0];
+      const stateCode = data.state_code;
+      try {
+        await db.query(
+            "INSERT INTO state_passports (user_id, state_id) SELECT $1, states.id FROM states WHERE states.state_code = $2",
+            [currentUserId, stateCode]
+        );
+        res.redirect("/");
+      } catch (err) {
+        console.log(err);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
+
 app.post("/user", async (req, res) => {
   currentUserId = req.body.user;
   res.redirect('/');
 });
 
 app.post("/switch", async (req, res) => {
-  console.log(req.body);
   if (req.body.map === 'world') {
-    const countries = await checkVisistedCountries();
-    res.render("index.ejs", {
-      countries: countries,
-      total: countries.length,
-      users: users,
-      color: users[currentUserId - 1].color,
-    });
+    currentMap = 1;
   } else if (req.body.map === 'united states') {
-    const states = await checkVisistedStates();
-    res.render("united_states.ejs", {
-      states: states,
-      total: states.length,
-      users: users,
-      color: users[currentUserId - 1].color,
-    });
+    currentMap = 2;
   } else {
     res.status(400).send("Unknown map state");
+    return;
   }
+
+  res.redirect("/");
 })
 
 // app.post("/new", async (req, res) => {
