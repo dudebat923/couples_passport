38d1679948e3bfe4235079fe7dca8e128cfca073
TylerBarrett-Werx <tbarrett@preswerx.com>
2024-10-01 12:27:33 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "12SeptembeR!@",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentUserId = 1;
+
+let users = [
+  { id: 1, name: "Together", color: "palevioletred" },
+  { id: 2, name: "Tyler", color: "mediumseagreen" },
+  { id: 3, name: "Monica", color: "dodgerblue" },
+];
+
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: "teal",
+  });
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
+    );
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/user", async (req, res) => {});
+
+app.post("/switch", async (req, res) => {
+  console.log(req.body);
+  if (req.body.map === 'world') {
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      users: users,
+      color: "teal",
+    });
+  } else if (req.body.map === 'united states') {
+    const countries = await checkVisisted();
+    res.render("united_states.ejs", {
+      countries: countries,
+      total: countries.length,
+      users: users,
+      color: "teal",
+    });
+  } else {
+    res.status(400).send("Unknown map state");
+  }
+})
+
+// app.post("/new", async (req, res) => {
+//   //Hint: The RETURNING keyword can return the data that was inserted.
+//   //https://www.postgresql.org/docs/current/dml-returning.html
+// });
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
